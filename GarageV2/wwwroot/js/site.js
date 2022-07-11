70a60b652e9bb05bd2626242df8259ec72cb8120
Mikael Klevengård <mickekleven@outlook.com>
2022-07-11 10:52:06 +0200

    Add vehicletype event in site.js

--- GarageV2/wwwroot/js/site.js
+++ GarageV2/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Triggers by onchange event in _VehicleTypeSelector
+const vehicleType = document.querySelector('#btnvehicletype').addEventListener('onclick', function () {
+
+    console.log('Testing')
+}); 
+
+
+
