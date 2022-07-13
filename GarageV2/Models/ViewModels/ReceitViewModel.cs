0f25eb853eb01f4e9884487fa3b86c958d157da3
Krister Magnusson <kristermagnusson@email.com>
2022-07-13 13:33:58 +0200

    fff

---
+++ GarageV2/Models/ViewModels/ReceitViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace GarageV2.Models.ViewModels
+{
+    using System.ComponentModel.DataAnnotations;
+    public class ReceitViewModel
+    {
+
+
+
+        [Display(Name = "Registrerings nummer")]
+        public String Reg { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd hh:mm}")]
+        [Display(Name = "Parkeringsdatum")]
+        public DateTime Arrival { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd hh:mm}")]
+        [Display(Name = "Utcheckningsdatum")]
+        public DateTime CheckOut { get; set; }
+
+       [ DisplayFormat(DataFormatString = @"{0:dd\:hh\:mm}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Parkerad tid")]
+        public TimeSpan ParkTime { get; set; }
+
+        [Display(Name = "Parkeringsavgift")]
+        public float ParkingPrice { get; set; }
+    }
+    }
